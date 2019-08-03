f11310fdcb58f0bd1bd99d222e0327ab43e5c48c
lczupryn <lczupryn@tibco.com>
2019-08-04 00:22:06 +0200

    add test

--- simpleApp.module/Tests/A3DEWS2RF4.ml
+++ simpleApp.module/Tests/A3DEWS2RF4.ml
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="ASCII"?>
 <emulation:BWTFile xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:emulation="http:///emulation.ecore">
-  <CurrentLoadedTest href="platform:/resource/simpleApp.module/Tests/simpleAppTest.bwt#/"/>
+  <CurrentLoadedTest href="platform:/resource/simpleApp.module/Tests/SubProcessTest.bwt#/"/>
   <EmulationDataFile>ACED000574001173696D706C65417070546573742E627774</EmulationDataFile>
+  <EmulationDataFile>ACED000574001253756250726F63657373546573742E627774</EmulationDataFile>
 </emulation:BWTFile>
