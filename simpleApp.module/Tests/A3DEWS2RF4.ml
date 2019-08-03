9e8b5c5acf261244611a0b189b27eb3ad566a35a
lczupryn <lczupryn@tibco.com>
2019-08-03 23:58:46 +0200

    add subprocess

---
+++ simpleApp.module/Tests/A3DEWS2RF4.ml
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="ASCII"?>
+<emulation:BWTFile xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:emulation="http:///emulation.ecore">
+  <CurrentLoadedTest href="platform:/resource/simpleApp.module/Tests/simpleAppTest.bwt#/"/>
+  <EmulationDataFile>ACED000574001173696D706C65417070546573742E627774</EmulationDataFile>
+</emulation:BWTFile>
